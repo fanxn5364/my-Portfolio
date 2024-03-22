7f93329510fcfaff09ba785b61889fae32b8c03e
Suparoek Manajit <72693473+fanxn5364@users.noreply.github.com>
2024-03-22 21:06:39 +0000

    Update personal information and images

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I am a student and currently studying at Bangkok University of Technology, Thailand. I always enjoy solving problems and learning new things.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new ways. and creative To drive innovation by leveraging my experience and knowledge. So I'm looking for new ways. We continually strive to make technology accessible to everyone. and create new things To get better all the time.";
 
 const About = () => {
   return (
